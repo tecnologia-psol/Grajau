643cf543b1687f3212d71c5be471538b83787900
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-05-08 16:56:44 -0300

    gdal

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from osgeo import gdal
+from osgeo import gdal, ogr
 import sys
 
 from_d = sys.argv[1]
 to_d = sys.argv[2]
 
 print(from_d, '->', to_d)
+
+g = gdal.Open("from/Acesso GIS/Acesso Raster.tif")
+h = ogr.Open("from/Acesso GIS/SaoPaulo Distritos com Area.gpkg")
+
+print('g',g)
+print('h',h)
+
+exit(0)
