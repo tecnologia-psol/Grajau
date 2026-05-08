9c2d8a54d866226d052deb4e019cdb3f7b10d6dd
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-05-08 17:28:14 -0300

    features

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from osgeo import gdal, ogr
-import sys
+import sys, os
+
+ogr.UseExceptions()
 
 from_d = sys.argv[1]
 to_d = sys.argv[2]
 
+if os.path.isdir("./tmp"):
+	os.rmdir("./tmp")
+os.mkdir("./tmp")
+
 print(from_d, '->', to_d)
 
-g = gdal.Open("from/Acesso GIS/Acesso Raster.tif")
-h = ogr.Open("from/Acesso GIS/SaoPaulo Distritos com Area.gpkg")
+distritos_vector: gdal.Dataset = ogr.Open("from/Acesso GIS/SaoPaulo Distritos com Area.gpkg")
+acesso_vector: gdal.Dataset = ogr.Open("from/Acesso GIS/acess_spo.gpkg")
+censo_vector: gdal.Dataset = ogr.Open("from/Acesso GIS/densidade_demografica.shp")
 
-print('g',g)
-print('h',h)
+acesso_layer: osgeo.ogr.Layer = acesso_vector.GetLayer(0)
 
-exit(0)
+for feature in acesso_layer:
+	print("FEAT ",feature)
+
+print("layer",type(acesso_layer),acesso_layer);
