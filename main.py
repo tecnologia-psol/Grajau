a2322937bedcf59e508e3c25b4da387e1a1da8f3
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-05-08 17:41:17 -0300

    coleta de dados

--- main.py
+++ main.py
@@ -18,7 +18,28 @@ censo_vector: gdal.Dataset = ogr.Open("from/Acesso GIS/densidade_demografica.shp
 
 acesso_layer: osgeo.ogr.Layer = acesso_vector.GetLayer(0)
 
-for feature in acesso_layer:
-	print("FEAT ",feature)
+tipos = {
+	"CMA": "Indicador de acessibilidade cumulativo ativo",
+	"CMP": "Indicador de acessibilidade cumulativo passivo",
+	"TMI": "Indicador de tempo mínimo até oportunidade mais próxima"
+}
 
-print("layer",type(acesso_layer),acesso_layer);
+modes = {}
+years = {}
+
+printf("Coletando lista de rótulos")
+for feature in acesso_layer:
+	# print("FEAT ",feature)
+	# print(feature.year,feature.mode)
+	if modes.get(feature.mode) == None:
+		modes[feature.mode] = feature.mode
+	pass
+	year_s = str(int(feature.year))
+	if years.get(year_s) == None:
+		years[year_s] = int(feature.year)
+	pass
+
+print(modes)
+print(years)
+
+# print("layer",type(acesso_layer),acesso_layer);
